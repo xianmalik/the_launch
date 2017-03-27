dda501d36754823df98672e3e585889162a372ed
Zubayer <haider15-5902@diu.edu.bd>
2017-03-28 00:51:55 +0600

    Particles

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 jQuery(document).ready(function($){
-    $("body").css('opacity', 1).addClass('active');
-    var elem = $('#Slider .slider-h span:first-child');
-    function typing(el){
-        if( !el.prev().is(':last-child') ){ el = el.css('opacity', 1).next(); }
-        var t = setTimeout(function() {typing(el)}, 100);
-    }
-    var t = setTimeout(function() {typing(elem)}, 500);
+    $(window).load(function(){
+        $("body").css('opacity', 1).addClass('active');
+        var elem = $('#Slider .slider-h span:first-child');
+        function typing(el){
+            if( !el.prev().is(':last-child') ){ el = el.css('opacity', 1).next(); }
+            var t = setTimeout(function() {typing(el)}, 100);
+        }
+        var t = setTimeout(function() {typing(elem)}, 500);
+    });
     var owl = $(".owl-carousel");
     $(".owl-carousel").owlCarousel({
         center: true,
